ed6f9c606aabd161b6c131f84524b1fa39a100fb
Andrius Martinkėnas <andrius.martinkenas321@gmail.com>
2022-08-05 11:01:13 +0300

    nieko gero

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
 
+    const [jokes, setJokes] = useState([]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>lalala</h1>
-      </header>
-      
-    </div>
-  );
+    useEffect(() => {
+
+        axios.get('https://v2.jokeapi.dev/joke/Programming?amount=10')
+        .then(res => setJokes(res.data));
+
+    },[]);
+ console.log(jokes);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ul>{
+                    jokes.map(u => u.jokes.type === 'twopart' ? <li key={u.jokes.id}> SETUP:{u.jokes.setup}<span style={{color: 'red'}}>DELIVERY: {u.jokes.delyvery}</span>,</li> : <li key={u.jokes.id}> JOKE:{u.jokes.joke}</li>)
+                    }
+                </ul>
+
+            </header>
+        </div>
+    );
 }
-
 export default App;
